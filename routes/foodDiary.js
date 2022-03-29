327d9edcbedc94d433e8666498c41afe0f88d4c2
Umesha-Nirmani <58287574+Umesha-Nirmani@users.noreply.github.com>
2022-03-29 13:07:03 +0530

    wrote functions to create graphs

--- routes/foodDiary.js
+++ routes/foodDiary.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
 const foodDiaryRouter = express.Router();
 const FoodDiary = require("../models/foodDiary");
+const Moment = require("moment");
+const MomentRange = require("moment-range");
+const moment =
+  MomentRange.extendMoment(Moment); /*add plugin to moment instance*/
+const _ = require("lodash");
 
 //test all
 foodDiaryRouter.get("/", async (req, res) => {
@@ -19,6 +27,7 @@ foodDiaryRouter.post("/create", async (req, res) => {
     FoodDetails: req.body.FoodDetails,
     totalMealCalorie: req.body.totalMealCalorie,
   });
+
   try {
     const newRecord = await foodDiary.save();
     res.status(200).json({
@@ -34,14 +43,100 @@ foodDiaryRouter.post("/create", async (req, res) => {
 foodDiaryRouter.post("/fetch", async (req, res) => {
   console.log("req food diary fetch: ", req.body);
   let selectedDate = req.body.Date;
+  let diaryData = await FoodDiary.find({ Date: selectedDate });
+  console.log("diaryData", diaryData);
+  if (diaryData === []) {
+    res.status(400).json({
+      status: "success",
+      message: "Records not found",
+      data: null,
+    });
+  }
+
   try {
-    let diaryData = await FoodDiary.find({ Date: selectedDate });
     res.status(200).json({
       status: "success",
       message: "Found Records",
       data: diaryData,
     });
-    if ((diaryData = null)) {
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// aggregation pipeline
+const graphData = async () => {
+  const pipeline = [
+    {
+      $group: {
+        _id: "$Date",
+        totalForDay: {
+          $sum: "$totalMealCalorie",
+        },
+      },
+    },
+    {
+      $sort: {
+        _id: 1,
+      },
+    },
+  ];
+  const result = await db.collection("fooddiaries").aggregate(pipeline);
+  const resultArray = await result.toArray();
+  //console.log("resultAsArray", resultArray);
+  return resultArray;
+};
+
+foodDiaryRouter.post("/fetchByTimeRange", async (req, res) => {
+  console.log("req food diary fetch: ", req.body);
+
+  // format array created in aggregation pipeline
+  const graphDataArray = await graphData();
+  //console.log("graphDataArray", graphDataArray);
+  let graphDataArrayFormatted = [];
+  for (let i = 0; i < graphDataArray.length; i++) {
+    graphDataArrayFormatted.push({
+      date: moment(graphDataArray[i]._id, "YYYY-MM-DD").format("YYYY-MM-DD"),
+      totCaloriePerDay: graphDataArray[i].totalForDay,
+    });
+  }
+  console.log("graphDataArrayFormatted", graphDataArrayFormatted);
+
+  // creating the array of dates within the given time range
+  let diaryData = await FoodDiary.find({
+    Date: { $gte: req.body.DateStart, $lte: req.body.DateEnd },
+  });
+  const start = moment(req.body.DateStart, "YYYY-MM-DD");
+  const end = moment(req.body.DateEnd, "YYYY-MM-DD");
+  const range = moment.range(start, end);
+  array = Array.from(range.by("days"));
+  let dateRange = [];
+  for (let i = 0; i < array.length; i++) {
+    dateRange.push({
+      date: moment(array[i], "YYYY-MM-DD").format("YYYY-MM-DD"),
+    });
+  }
+  console.log("dateRange", dateRange);
+
+  //match the 2 arrays and create the resulting data set
+  let finalDataSet = [];
+  for (let i = 0; i < dateRange.length; i++) {
+    let result = _.find(graphDataArrayFormatted, { date: dateRange[i].date });
+    if (result === undefined) {
+      finalDataSet.push({ date: dateRange[i].date, totCaloriePerDay: 0 });
+    } else {
+      finalDataSet.push(result);
+    }
+  }
+  console.log("finalDataSet", finalDataSet);
+
+  try {
+    res.status(200).json({
+      status: "success",
+      message: "Found Records",
+      data: finalDataSet,
+    });
+    if (diaryData == null) {
       res.status(200).json({
         status: "success",
         message: "Records not fount",
